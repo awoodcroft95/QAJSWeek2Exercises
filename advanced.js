a5e2288bec36921560b4009cc1099f6bca81dc9f
awoodcroft95 <awoodcroft95@gmail.com>
2017-11-08 13:54:52 +0000

    completed making my own api

--- advanced.js
+++ advanced.js
@@ -2,10 +2,10 @@ let _ = require("lodash");
 
 let itemArray = [];
 let clock = { name: "clock", weight: 2, value: 10 };
-let sword = { name: "sword", weight: 10, value: 50};
-let shield = {name: "shield", weight: 20, value: 30};
-let food = {name: "food", weight: 5, value: 10};
-let fancyFood = {name: "fancy food", weight: 5, value: 20};
+let sword = { name: "sword", weight: 10, value: 50 };
+let shield = { name: "shield", weight: 20, value: 30 };
+let food = { name: "food", weight: 5, value: 10 };
+let fancyFood = { name: "fancy food", weight: 5, value: 20 };
 
 itemArray.push(clock);
 itemArray.push(sword);
@@ -16,5 +16,11 @@ itemArray.push(fancyFood);
 fillLootSack(30, itemArray);
 
 function fillLootSack(size, items) {
-    
+    let spaceLeft = size;
+    let isSpaceLeft = true;
+    while (isSpaceLeft){
+        for (let item in items){
+            
+        }
+    }
 }
---
+++ apiServer.js
@@ -0,0 +1,103 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const open = require("open");
+
+const app = express();
+const port = process.env.port || 3000;
+const router = express.Router();
+
+//Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//Routing
+app.use("/", router);
+
+//GET Requests
+
+router.get("/api/ship/", (req, res) => {
+    res.json(ships);
+});
+
+
+router.get("/api/ship/:id", (req, res) => {
+    const shipID = req.params.id;
+    const currentShip = ships.find((ship) => ship.id == shipID);
+    if (currentShip) { res.json(currentShip); }
+    else { res.sendStatus(404); }
+});
+
+//POST Requests
+router.post("/api/ship/", (req, res) => {
+    const postShip = req.body;
+    console.log(postShip);
+    console.log(isValidShip(postShip) +""+ !ships.find((a) => a.id == postShip.id))
+    const isValid = isValidShip(postShip) && !ships.find((a) => a.id == postShip.id);
+    console.log(isValid);
+    if (isValid) {
+        ships.push(postShip);
+        res.send(postShip);
+    }
+    else { res.sendStatus(500) }
+});
+
+//PUT Requests
+router.put("/api/ship/:id", (req, res) => {
+    const shipID = req.params.id;
+    const currentShip = ships.find((ship) => ship.id == shipID);
+    if (currentShip) {
+        const putShip = req.body;
+        const isValid = isValidShip(putShip);
+        if (isValid) {
+            currentShip.name = putShip.name;
+            currentShip.speed = putShip.speed;
+            currentShip.minCrew = putShip.minCrew;
+            currentShip.length = putShip.length;
+            currentShip.passengers = putShip.passengers;
+            res.sendStatus(204);
+        }
+        else { res.sendStatus(404); }
+    }
+});
+
+//DELETE Request
+router.delete("/api/ship/:id", (req, res) => {
+    const shipID = req.params.id;
+    const currentShip = ships.findIndex((ship) => ship.id == shipID);
+    if (currentShip !== -1) {
+        ships.splice(currentShip, 1);
+        res.sendStatus(204);
+    }
+    else {
+        res.sendStatus(404);
+    }
+});
+
+//mock data
+const ships = [
+    {
+        id: 1,
+        name: "Millennium Falcon",
+        speed: 1050,
+        minCrew: 1,
+        length: 34.37,
+        passengers: 6
+    },
+    {
+        id: 2,
+        name: "Ghost",
+        speed: 1025,
+        minCrew: 1,
+        length: 43.9,
+        passengers: 8
+    }
+]
+
+function isValidShip(ship) {
+    return "id" in ship && "name" in ship && "speed" in ship && "minCrew" in ship && "length" in ship && "passengers" in ship;
+}
+
+app.listen(port, (err) => {
+    if (err) { console.log(err); }
+    else { open(`http://localhost:${port}`) }
+});
