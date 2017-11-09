ac54cc8b4204057189a81d5ead4fcc003a4d98ac
awoodcroft95 <awoodcroft95@gmail.com>
2017-11-09 17:06:33 +0000

    MAde the page look nice

--- examplename/src/App.js
+++ examplename/src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import logo from './Redstarbird.png';
 import './App.css';
 
 class App extends Component {
@@ -7,15 +7,34 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h1 className="App-title">STAR WARS SHIP API</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Hello!
-        </p>
+        <div>
+        <input id="inputShipName" type="text" placeholder="Enter a name"></input><br></br>
+        <input id="inputShipSpeed" type="number" placeholder="Enter a speed"></input><br></br>
+        <input id="inputShipCrew" type="number" placeholder="Enter minimum crew size"></input><br></br>
+        <input id="inputShipLength" type="number" placeholder="Enter ship length"></input><br></br>
+        <input id="inputShipPassengers" type="number" placeholder="Enter max number of passengers"></input><br></br>
+        <input type="button" onclick="createAndAppendDog()" value="Create"></input>
+        <input type="button" onclick="updateAndAppendDog()" value="Update"></input><br></br>
+        <div id="idStore" value=""></div>
+        </div>
+        <div class="animate-bottom">
+          <table id="shipTable">
+            <h2>Ships</h2>
+            <table>
+              <thead>
+                <th>Name</th>
+                <th>Speed</th>
+                <th>Minimum Crew</th>
+                <th>Length</th>
+                <th>Passengers</th>
+              </thead>
+              <tbody id="shipDetails"></tbody>
+            </table>
+          </table>
+        </div>
       </div>
     );
   }
