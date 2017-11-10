880871045a8c9fe16ead0add87e792a04bae1e70
awoodcroft95 <awoodcroft95@gmail.com>
2017-11-10 10:35:52 +0000

    added millennium falcon to objArray as test data

--- examplename/src/index.js
+++ examplename/src/index.js
@@ -6,11 +6,20 @@ import registerServiceWorker from './registerServiceWorker';
 
 const objArray = [
     {
+        id: 0,
         name: "X-Wing",
         speed: 1050,
         minCrew: 1,
         length: 12,
         passengers: 1
+    },
+    {
+        id: 1,
+        name: "Millennium Falcon",
+        speed: 1050,
+        minCrew: 1,
+        length: 34.37,
+        passengers: 6
     }
 ];
 
